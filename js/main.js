befbccd7806d6834f2140bf227e249da0212778d
Raúl Reséndiz <rresendizlazaro@hotmail.com>
2020-07-11 10:07:16 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,43 +1,54 @@
 //Jquery
 $(document).ready(function(){
+    /*
     //Post
     if(window.location.href.indexOf('index') > -1){
         var posts = [
             {
                 title: '¿Quién soy yo?',
-                content: 'Soy una persona honesta y responsable, me gusta desarrollar proyectos en los que invoucre la mayor capacidad de mis conocimientos, dentro de estos hago todo lo posible por hacer efectivo el tiempo de desarrollo y cumplir con fechas de entrega, prefiero tomar la mayor parte de las responsabilidades incluyendo ser el lider, ya que se me facilita la organización y distribución de los roles de cada integrante.'
+                content: ,
+                image: ''
             },
             {
                 title: 'Historia Académica',
-                content: 'Estudié Ingeniería mecatrónica en la <b>Facultad de Ingeniería</b> de la <b>UNAM</b>.<br>Promedio: <b>8.92</b><br>En este momento me encuentro en proceso de titulación por medio de tesis. Con el tema <b>"Circuito carciovascular para pruebas de válvulas cardiacas"</b>, a grandes resgos es la implementación de una interfaz gráfica para el control de un dispositivo probador de válvulas cardiacas.',
+                content: '',
                 image: '<img src="img/unam.png">'
             },
             {
                 title: 'Objetivo profesional',
-                content: 'Formar parte de una empresa o institución enfocada a la ingeniería, para brindar soluciones eficaces en mecánica, eléctrica, electrónica y computación, que permitan desarrollarme en el ámbito profesional.'
+                content: '',
+                image: ''
             }
         ]
         posts.forEach((item, index)=> {
             var post = `
-                <article class="post">
-                    <h2>${item.title}</h2>
-                    <p>
-                        ${item.content}
-                    </p>
-                    <p>
-                        ${item.image}
-                    </p>
-                    <hr>
-                </article>
+            <article class="post">
+                <h2>${item.title}</h2>
+                <p>
+                    ${item.content}
+                </p>
+                <p>
+                    ${item.image}
+                </p>
+                <hr>
+            </article>
             `;
             $("#post").append(post);
         })
     }
+    */
 
     //Link
     $("#linkedin").click(function(){
-        window.open("https://www.linkedin.com/in/ra%C3%BAl-eduardo-res%C3%A9ndiz-l%C3%A1zaro-540002192/?originalSubdomain=mx");
+        window.open("https://www.linkedin.com/in/ra%C3%BAl-eduardo-res%C3%A9ndiz-l%C3%A1zaro-540002192/");
     });
+
+    //Acordeón
+    if(window.location.href.indexOf('conocimiento') > -1){
+        $("#acordeon").accordion({
+            collapsible: true
+        });
+    }
 
     //Scroll arriba
     $(".subir").click(function(e){
